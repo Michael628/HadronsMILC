96d8184077845129c12e53e2f2a46cdd3376d796
Michael628 <michaellynch628@gmail.com>
2025-02-20 11:11:55 -0800

    Signature bug fix for input_params function

--- utilities/python_scripts/src/python_scripts/nanny/spawnjob.py
+++ utilities/python_scripts/src/python_scripts/nanny/spawnjob.py
@@ -153,7 +153,7 @@ def make_inputs(param, step, cfgno_steps):
         input_file: str = job_config.get_infile(submit_config)
 
         # TODO: Move input file creation into runio module
-        input_params, schedule = config.input_params(job_config, outfile_config)
+        input_params, schedule = config.input_params(job_config, submit_config, outfile_config)
 
         if job_config.job_type == 'contract':
             input_string = yaml.dump(input_params)
