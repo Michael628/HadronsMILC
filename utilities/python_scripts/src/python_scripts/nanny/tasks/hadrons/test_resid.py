b78020f75d60d774bbbfd63c6cb8cf4fbef9824f
Michael Lynch <michaellynch628@gmail.com>
2025-03-31 12:03:53 -0700

    hack: test_resid task

--- utilities/python_scripts/src/python_scripts/nanny/tasks/hadrons/test_resid.py
+++ utilities/python_scripts/src/python_scripts/nanny/tasks/hadrons/test_resid.py
@@ -11,106 +11,29 @@ from python_scripts import Gamma, utils
 from python_scripts.nanny import TaskBase
 from python_scripts.nanny.config import OutfileList
 from python_scripts.nanny.tasks.hadrons import SubmitHadronsConfig, templates
+from python_scripts.nanny.tasks.hadrons.lmi import LMITask
 
 
-# ============LMI Task Configuration===========
+# ============TestResid Task Configuration===========
 @dataclass
-class LMITask(TaskBase):
+class TestResidTask(TaskBase):
     # ============Epack===========
-    @dataclass
-    class EpackTask(TaskBase):
-        load: bool
-        multifile: bool = False
-        save_eigs: bool = False
-        save_evals: bool = True
 
-    # ============Operator List===========
-    @dataclass
-    class OpList(TaskBase):
-        """Configuration for a list of gamma operations.
+    epack: t.Optional[LMITask.EpackTask] = None
+    high_modes: t.Optional[LMITask.HighModes] = None
+    resid: t.Optional[t.List[float]] = None
 
-        Attributes
-        ----------
-        operations: list
-            Gamma operations to be performed, usually for meson fields or high mode solves.
-        """
+    def __post_init__(self):
+        assert isinstance(self.resid, t.List) and len(self.resid) > 0
+        assert all([isinstance(r, float) for r in self.resid])
 
-        @dataclass
-        class Op:
-            """Parameters for a gamma operation and associated masses."""
+        assert not self.high_modes.skip_cg
 
-            gamma: Gamma
-            mass: t.List[str]
-
-        operations: t.List[Op]
-
-        @classmethod
-        def from_dict(cls, kwargs) -> "LMITask.OpList":
-            """Creates a new instance of OpList from a dictionary.
-
-            Note
-            ----
-            Valid dictionary input formats:
-
-            kwargs = {
-              'gamma': ['op1','op2','op3'],
-              'mass': ['m1','m2']
-            }
-
-            or
-
-            kwargs = {
-              'op1': {
-                'mass': ['m1']
-              },
-              'op2': {
-                'mass': ['m2','m3']
-              }
-            }
-
-            """
-            params = utils.deep_copy_dict(kwargs)
-
-            if "mass" not in params:
-                operations = []
-                for key, val in params.items():
-                    mass = val["mass"]
-                    if isinstance(mass, str):
-                        mass = [mass]
-                    gamma = Gamma[key.upper()]
-                    operations.append(cls.Op(gamma=gamma, mass=mass))
-            else:
-                gammas = params["gamma"]
-                mass = params["mass"]
-                if isinstance(mass, str):
-                    mass = [mass]
-                if isinstance(gammas, str):
-                    gammas = [gammas]
-                operations = [cls.Op(gamma=Gamma[g.upper()], mass=mass) for g in gammas]
-            params["operations"] = operations
-            return cls(**params)
-
-        @property
-        def mass(self):
-            res: t.Set = set()
-            for op in self.operations:
-                for m in op.mass:
-                    res.add(m)
-
-            return list(res)
-
-    @dataclass
-    class HighModes(OpList):
-        skip_cg: bool = False
-        solver: str = "mpcg"
-
-    epack: t.Optional[EpackTask] = None
-    meson: t.Optional[OpList] = None
-    high_modes: t.Optional[HighModes] = None
+        self.resid = sorted(self.resid, reverse=True)
 
     @classmethod
-    def from_dict(cls, kwargs) -> "LMITask":
-        """Creates a new instance of LMITaskConfig from a dictionary."""
+    def from_dict(cls, kwargs) -> "TestResidTask":
+        """Creates a new instance of TestResidTaskConfig from a dictionary."""
         params = utils.deep_copy_dict(kwargs)
         for f in fields(cls):
             field_name = f.name
@@ -127,8 +50,6 @@ class LMITask(TaskBase):
 
         if self.epack and not self.epack.load:
             res.append("zero")
-        if self.meson:
-            res += self.meson.mass
         if self.high_modes:
             res += self.high_modes.mass
 
@@ -137,104 +58,13 @@ class LMITask(TaskBase):
 
 # ============Functions for building params and checking outfiles===========
 def input_params(
-    tasks: LMITask, submit_config: SubmitHadronsConfig, outfile_config_list: OutfileList
+    tasks: TestResidTask,
+    submit_config: SubmitHadronsConfig,
+    outfile_config_list: OutfileList,
 ) -> t.Tuple[t.List[t.Dict], t.Optional[t.List[str]]]:
-    def build_schedule(module_names: t.List[str]) -> t.List[str]:
-        gammas = ["pion_local", "vec_local", "vec_onelink"]
-
-        def pop_conditional(mi, cond):
-            indices = [i for i, item in enumerate(mi) if cond(item)]
-            # Pop in reverse order but return in original order
-            return [mi.pop(i) for i in indices[::-1]][::-1]
-
-        def get_mf_inputs(x):
-            is_action = x["type"].endswith("ImprovedStaggeredMILC")
-            is_evec = "ModifyEigenPack" in x["type"]
-            has_sea_mass = "mass_l" in x["name"]
-            return (is_action or is_evec) and has_sea_mass
-
-        dp_gauges = pop_conditional(module_names, lambda x: "LoadIldg" in x["type"])
-        sp_gauges = pop_conditional(
-            module_names, lambda x: "PrecisionCast" in x["type"]
-        )
-        meson_fields = pop_conditional(
-            module_names, lambda x: "A2AMesonField" in x["type"]
-        )
-        meson_field_inputs = pop_conditional(module_names, get_mf_inputs)
-
-        indep_mass_tslice = pop_conditional(
-            module_names,
-            lambda x: ("mass" not in x["name"] or "mass_zero" in x["name"])
-            and "_t" not in x["name"],
-        )
-
-        sorted_modules = dp_gauges + indep_mass_tslice
-        sorted_modules += meson_field_inputs + meson_fields
-        sorted_modules += sp_gauges
-
-        def gamma_order(x):
-            for i, gamma in enumerate(gammas):
-                if gamma in x["name"]:
-                    return i
-            return -1
-
-        def mass_order(x):
-            for i, mass in enumerate(submit_config.mass.keys()):
-                if f"mass_{mass}" in x["name"]:
-                    return i
-            return -1
-
-        def mixed_mass_last(x):
-            return len(re.findall(r"_mass", x["name"]))
-
-        def tslice_order(x):
-            time = re.findall(r"_t(\d+)", x["name"])
-            if len(time):
-                return int(time[0])
-            else:
-                return -1
-
-        module_names = sorted(module_names, key=gamma_order)
-        module_names = sorted(module_names, key=mass_order)
-        module_names = sorted(module_names, key=mixed_mass_last)
-        module_names = sorted(module_names, key=tslice_order)
-
-        sorted_modules += module_names
-
-        return [m["name"] for m in sorted_modules]
-
     submit_conf_dict = submit_config.string_dict()
 
-    if not submit_config.overwrite_sources:
-        if tasks.high_modes:
-            cf = catalog_files(tasks, submit_config, outfile_config_list)
-            missing_files = cf[cf["exists"] != True]
-            run_tsources = []
-            for tsource in submit_config.tsource_range:
-                if any(missing_files["tsource"] == str(tsource)):
-                    run_tsources.append(str(tsource))
-
-        if tasks.meson:
-            bf = bad_files(tasks, submit_config, outfile_config_list)
-            meson_template = outfile_config_list.meson_ll.filename
-            for i, op in enumerate(tasks.meson.operations[:]):
-                for j, mass_label in enumerate(op.mass[:]):
-                    meson_files = [
-                        meson_template.format(
-                            mass=submit_config.mass_out_label[mass_label],
-                            gamma=g,
-                            **submit_conf_dict,
-                        )
-                        for g in op.gamma.gamma_list
-                    ]
-                    if not any([mf in bf for mf in meson_files]):
-                        op.mass.pop(j)
-
-                if not op.mass:
-                    tasks.meson.operations.pop(i)
-
-    else:
-        run_tsources = list(map(str, submit_config.tsource_range))
+    run_tsources = list(map(str, submit_config.tsource_range))
 
     gauge_filepath = outfile_config_list.gauge_links.filestem.format(**submit_conf_dict)
     gauge_fat_filepath = outfile_config_list.fat_links.filestem.format(
@@ -261,11 +91,7 @@ def input_params(
             )
         )
 
-    if (
-        tasks.high_modes
-        and not tasks.high_modes.skip_cg
-        and tasks.high_modes.solver == "mpcg"
-    ):
+    if tasks.high_modes and tasks.high_modes.solver == "mpcg":
         for mass_label in tasks.high_modes.mass:
             name = f"istag_mass_{mass_label}"
             mass = str(submit_config.mass[mass_label])
@@ -330,30 +156,6 @@ def input_params(
                 )
             )
 
-    if tasks.meson:
-        meson_template = outfile_config_list.meson_ll.filestem
-        for op in tasks.meson.operations:
-            op_type = op.gamma.name.lower()
-            gauge = "" if op.gamma == Gamma.LOCAL else "gauge"
-            for mass_label in op.mass:
-                output = meson_template.format(
-                    mass=submit_config.mass_out_label[mass_label], **submit_conf_dict
-                )
-                modules.append(
-                    templates.meson_field(
-                        name=f"mf_{op_type}_mass_{mass_label}",
-                        action=f"stag_mass_{mass_label}",
-                        block=submit_conf_dict["blocksize"],
-                        gammas=op.gamma.gamma_string,
-                        apply_g5="false",
-                        gauge=gauge,
-                        low_modes=f"evecs_mass_{mass_label}",
-                        left="",
-                        right="",
-                        output=output,
-                    )
-                )
-
     if tasks.high_modes:
 
         def m1_eq_m2(x):
@@ -377,36 +179,35 @@ def input_params(
         solver_labels = []
         if tasks.epack:
             solver_labels.append("ranLL")
-        if tasks.high_modes and not tasks.high_modes.skip_cg:
-            solver_labels.append("ama")
+
+        for res in tasks.resid:
+            solver_labels.append(f"ama_{res}")
 
         high_path = outfile_config_list.high_modes.filestem
+
         for op in tasks.high_modes.operations:
             glabel = op.gamma.name.lower()
-            quark_iter = list(
-                itertools.product(run_tsources, solver_labels, op.mass, op.mass)
-            )
+            mlabel = op.mass
 
-            for tsource, slabel, mlabel, _ in filter(m1_eq_m2, quark_iter):
-                quark = f"quark_{slabel}_{glabel}_mass_{mlabel}_t{tsource}"
-                source = f"noise_t{tsource}"
-                solver = f"stag_{slabel}_mass_{mlabel}"
-                if slabel == "ama" and tasks.epack:
-                    guess = f"quark_ranLL_{glabel}_mass_{mlabel}_t{tsource}"
-                else:
-                    guess = ""
+            for tsource in run_tsources:
+                guess = ""
+                for slabel in solver_labels:
+                    quark = f"quark_{slabel}_{glabel}_mass_{mlabel}_t{tsource}"
+                    source = f"noise_t{tsource}"
+                    solver = f"stag_{slabel}_mass_{mlabel}"
 
-                modules.append(
-                    templates.quark_prop(
-                        name=quark,
-                        source=source,
-                        solver=solver,
-                        guess=guess,
-                        gammas=op.gamma.gamma_string,
-                        apply_g5="true",
-                        gauge="" if op.gamma.local else "gauge",
+                    modules.append(
+                        templates.quark_prop(
+                            name=quark,
+                            source=source,
+                            solver=solver,
+                            guess=guess,
+                            gammas=op.gamma.gamma_string,
+                            apply_g5="true",
+                            gauge="" if op.gamma.local else "gauge",
+                        )
                     )
-                )
+                    guess = quark
 
             for tsource, slabel, m1label, m2label in filter(m1_ge_m2, quark_iter):
                 quark1 = f"quark_{slabel}_{glabel}_mass_{m1label}_t{tsource}"
@@ -446,13 +247,13 @@ def input_params(
                 )
 
         for mass_label in tasks.high_modes.mass:
-            if "ama" in solver_labels:
+            for res in tasks.resid:
                 if tasks.high_modes.solver == "rb":
                     modules.append(
                         templates.rb_cg(
                             name=f"stag_ama_mass_{mass_label}",
                             action=f"stag_mass_{mass_label}",
-                            residual=submit_conf_dict["cg_residual"],
+                            residual=str(res),
                         )
                     )
                 else:
@@ -461,7 +262,7 @@ def input_params(
                             name=f"stag_ama_mass_{mass_label}",
                             outer_action=f"stag_mass_{mass_label}",
                             inner_action=f"istag_mass_{mass_label}",
-                            residual=submit_conf_dict["cg_residual"],
+                            residual=str(res),
                         )
                     )
 
@@ -474,14 +275,13 @@ def input_params(
                     )
                 )
 
-    module_info = [m["id"] for m in modules]
-    schedule = build_schedule(module_info)
+    schedule = []
 
     return modules, schedule
 
 
 def catalog_files(
-    task_config: LMITask,
+    task_config: TestResidTask,
     submit_config: SubmitHadronsConfig,
     outfile_config_list: OutfileList,
 ) -> pd.DataFrame:
@@ -497,13 +297,6 @@ def catalog_files(
                     yield {}, outfile_config_list.eig
             if task_config.epack.save_eigs:
                 yield {}, outfile_config_list.eval
-
-        if task_config.meson:
-            res: t.Dict = {}
-            for op in task_config.meson.operations:
-                res["gamma"] = op.gamma.gamma_list
-                res["mass"] = [submit_config.mass_out_label[m] for m in op.mass]
-                yield res, outfile_config_list.meson_ll
 
         if task_config.high_modes:
             res = {"tsource": list(map(str, submit_config.tsource_range)), "dset": []}
@@ -553,7 +346,7 @@ def catalog_files(
 
 
 def bad_files(
-    task_config: LMITask,
+    task_config: TestResidTask,
     submit_config: SubmitHadronsConfig,
     outfile_config_list: OutfileList,
 ) -> t.List[str]:
@@ -562,7 +355,7 @@ def bad_files(
 
 
 def processing_params(
-    task_config: LMITask,
+    task_config: TestResidTask,
     submit_config: SubmitHadronsConfig,
     outfile_config_list: OutfileList,
 ) -> t.Dict:
@@ -621,4 +414,4 @@ def processing_params(
 
 
 def get_task_factory():
-    return LMITask.from_dict
+    return TestResidTask.from_dict
